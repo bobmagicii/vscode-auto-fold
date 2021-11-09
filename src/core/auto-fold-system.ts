db25d44a1081cd01de0c0c8f9a0d70c2ff74632a
bobmagicii <bob@majdak.net>
2021-11-09 15:38:51 -0600

    looks like my plans will work

--- src/core/auto-fold-system.ts
+++ src/core/auto-fold-system.ts
@@ -2,8 +2,17 @@ import * as vscode from 'vscode';
 import Util from './util';
 
 class AutoFoldSystem {
+/*//
+@date 2021-11-09
+this is the primary management where all the decisions about what happens and
+when it happens occur. an instance of this is created and managed by the
+extension directly as its core interface.
+//*/
 
 	constructor() {
+	/*//
+	@date 2021-11-09
+	//*/
 
 		Util.println(
 			'Auto Fold System Initialized',
@@ -13,8 +22,47 @@ class AutoFoldSystem {
 		return;
 	};
 
-	public OnSwitchedEditors(editor: vscode.TextEditor | undefined):
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	public fold():
+	boolean {
+
+		let editor = vscode.window.activeTextEditor;
+
+		if(editor === undefined) {
+			Util.println('nothing to fold','AutoFoldSystem::fold');
+			return false;
+		}
+
+		Util.println(
+			'TODO: fold the visible document',
+			'AutoFoldSystem::fold'
+		);
+
+		// ...
+
+		return true;
+	};
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	public onCommandFold():
 	void {
+	/*//
+	@date 2021-11-09
+	//*/
+
+		this.fold();
+		return;
+	};
+
+	public onSwitchedEditors(editor: vscode.TextEditor | undefined):
+	void {
+	/*//
+	@date 2021-11-09
+	//*/
 
 		if(editor === undefined) {
 			Util.println('OnSwitchedEditors: none');
@@ -22,9 +70,13 @@ class AutoFoldSystem {
 		}
 
 		Util.println(`OnSwitchedEditors: ${editor.document.uri.fsPath}`);
+		this.fold();
 
 		return;
 	};
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
 
 };
 
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,43 @@
 import * as vscode from 'vscode';
 
 import Util from './core/util';
-//import AutoFoldSystem from './core/auto-fold-system';
+import AutoFoldSystem from './core/auto-fold-system';
 
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 
 export function activate(context: vscode.ExtensionContext) {
+/*//
+@date 2021-11-09
+@activation onStartupFinished
+//*/
 
 	Util.println('Loading Auto Fold...');
 
-	//let afSystem = new AutoFoldSystem;
+	let afSystem = new AutoFoldSystem;
+
+	////////
 
 	(context.subscriptions)
 	.push(
 		(vscode.commands)
 		.registerCommand(
 			'autofoldyeah.autoFold',
-			(() => vscode.window.showInformationMessage('TODO: execute the command.'))
+			afSystem.onCommandFold.bind(afSystem)
 		)
 	);
+
+	(vscode.window)
+	.onDidChangeActiveTextEditor(
+		afSystem.onSwitchedEditors.bind(afSystem)
+	);
+
+	////////
+
+	if(vscode.window.activeTextEditor !== undefined)
+	afSystem.onCommandFold();
+
+	////////
 
 	Util.println('Auto Fold Ready.');
 	return;
